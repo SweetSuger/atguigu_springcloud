5c1097e9e9b865005e408c01120427f290c55518
yinhao <yinhao19950121@163.com>
2021-01-12 17:30:33 +0800

    20210112   新系统初步提交

--- cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springcloud/controller/OrderController.java
@@ -29,7 +29,7 @@ public class OrderController {
 
     @GetMapping("/consumer/payment/get/{id}")
     public CommonResult<Payment> getPayment(@PathVariable("id") Integer id){
-        System.out.println("查询热部署测试");
+        System.out.println("查询热部署测试1");
         return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
     }
 }
