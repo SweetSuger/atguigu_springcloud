37b4f2ac1938a95e289d3b47a786a22ac37386e0
yinhao <yinhao19950121@163.com>
2021-01-28 16:12:17 +0800

    2021.01.28   eureka集群保护机制和zookeeper

--- cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8002/src/main/java/com/atguigu/springcloud/controller/PaymentController.java
@@ -6,7 +6,12 @@ import com.atguigu.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @author yinhao
@@ -20,6 +25,9 @@ public class PaymentController {
 
     @Value("${server.port}")
     private String serverPort;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @PostMapping(value = "/payment/create")
     public CommonResult insertPayment(@RequestBody Payment payment){
@@ -42,4 +50,20 @@ public class PaymentController {
             return new CommonResult(201,"没有记录！",null);
         }
     }
+
+    @GetMapping(value = "/payment/discovery")
+    public Object discovery(){
+        List<String> list = discoveryClient.getServices();
+        for (String s : list) {
+            log.info("**********discovery.element:" + s);
+        }
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance instance : instances) {
+            log.info(instance.getInstanceId()+"\t"+instance.getHost()+"\t"+instance.getPort()+"\t"+instance.getUri());
+        }
+
+        return this.discoveryClient;
+    }
+
 }
--- cloud-provider-payment8004/src/main/java/com/atguigu/springcloud/PaymentMain8004.java
+++ cloud-provider-payment8004/src/main/java/com/atguigu/springcloud/PaymentMain8004.java
@@ -3,17 +3,15 @@ package com.atguigu.springcloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
- * Created by yh on 2020/12/28.
+ * @author yinhao
+ * @date 2021/1/27 15:40
  */
-@EnableDiscoveryClient
-@EnableEurekaClient
 @SpringBootApplication
-public class PaymentMain8001 {
-
+@EnableDiscoveryClient //该注解用于向使用consul或者zookeeper作为注册中心时注册服务
+public class PaymentMain8004 {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class,args);
+        SpringApplication.run(PaymentMain8004.class,args);
     }
 }
